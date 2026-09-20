abc40f53871face8cd3ac1472f81a3f7d08b4d0c
Abdul-Ahaddd <abdulahadrandhawa127@gmail.com>
2026-09-20 16:18:13 +0500

    Handle additional edge cases

--- sorted_test.cpp
+++ sorted_test.cpp
@@ -77,10 +77,12 @@ int main()
 
 bool isSorted(const int *arr, const int size)
 {
+    if (size <= 1)
+        return true;
     for (int i = 0; i < size - 1; i++)
     {
         if (arr[i] > arr[i + 1])
-            return false; // fixed
+            return false;
     }
     return true;
 }
